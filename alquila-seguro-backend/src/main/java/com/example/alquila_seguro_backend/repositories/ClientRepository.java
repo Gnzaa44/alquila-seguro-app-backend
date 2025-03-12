eb755c55b80a6c65665edda12b8e4f71c2d38b4e
Gnzaa44 <141690430+Gnzaa44@users.noreply.github.com>
2025-03-12 11:36:04 -0300

    Update in repos (Client-Reservation)
    new methods for Client and Reservation

---
+++ alquila-seguro-backend/src/main/java/com/example/alquila_seguro_backend/repositories/ClientRepository.java
@@ -0,0 +1,13 @@
+package com.example.alquila_seguro_backend.repositories;
+
+import com.example.alquila_seguro_backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ClientRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
--- alquila-seguro-backend/src/main/java/com/example/alquila_seguro_backend/repositories/ReservationRepository.java
+++ alquila-seguro-backend/src/main/java/com/example/alquila_seguro_backend/repositories/ReservationRepository.java
@@ -1,7 +1,9 @@
 package com.example.alquila_seguro_backend.repositories;
 
+import com.example.alquila_seguro_backend.entity.Client;
 import com.example.alquila_seguro_backend.entity.Reservation;
-import com.example.alquila_seguro_backend.entity.User;
+import com.example.alquila_seguro_backend.entity.ReservationStatus;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +11,6 @@ import java.util.List;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-    List<Reservation> findByUser(User user);
-    List<Reservation> findByStatus(String status);
+    List<Reservation> findByClient(Client client);
+    List<Reservation> findByStatus(ReservationStatus status);
 }
