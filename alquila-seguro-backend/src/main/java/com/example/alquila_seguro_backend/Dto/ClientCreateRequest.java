f8c32125849df42ee9da9c258c80d5702e5824d9
Gnzaa44 <141690430+Gnzaa44@users.noreply.github.com>
2025-03-13 09:56:35 -0300

    Create ClientCreateRequest DTO
    Dto for Client Request

---
+++ alquila-seguro-backend/src/main/java/com/example/alquila_seguro_backend/Dto/ClientCreateRequest.java
@@ -0,0 +1,33 @@
+package com.example.alquila_seguro_backend.Dto;
+
+import com.example.alquila_seguro_backend.validation.ArgentinianPhoneNumber;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClientCreateRequest {
+    @NotBlank(message = "First name is required")
+    @Size(min = 3, max = 100, message = "First name must be between 3 and 100 characters long")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Size(min = 3, max = 100, message = "Last name must be between 3 and 100 characters long")
+    private String lastName;
+
+    @Email(message = "Email must be valid")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @ArgentinianPhoneNumber(message = "Phone number must be a valid Argentinian phone number")
+    @NotBlank(message = "Phone is required")
+    @Size(min = 10, max = 15, message = "Phone must be between 10 and 15 characters long")
+    private String phone;
+}
