85dd8b295be16bee0be23b81bd2fe6b817157917
Gnzaa44 <141690430+Gnzaa44@users.noreply.github.com>
2025-03-13 10:04:08 -0300

    Reservation DTO
    for ReservationRequest

---
+++ alquila-seguro-backend/src/main/java/com/example/alquila_seguro_backend/Dto/ReservationCreateRequest.java
@@ -0,0 +1,31 @@
+package com.example.alquila_seguro_backend.Dto;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReservationCreateRequest {
+    @NotNull(message = "Property ID is required")
+    private Long propertyId;
+
+    @NotNull(message = "Client ID is required")
+    private Long clientId;
+
+    @FutureOrPresent(message = "Start date must be today or in the future")
+    @NotNull(message = "Start date is required")
+    private LocalDateTime startDate;
+
+    @Future(message = "End date must be in the future")
+    @NotNull(message = "End date is required")
+    private LocalDateTime endDate;
+}
