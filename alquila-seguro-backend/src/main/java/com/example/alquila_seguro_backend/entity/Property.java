1627a437778b5402f4e60d0684336e85d847b701
Gnzaa44 <141690430+Gnzaa44@users.noreply.github.com>
2025-03-20 10:29:41 -0300

    Update Property
    Added enums in Property and language changes

--- alquila-seguro-backend/src/main/java/com/example/alquila_seguro_backend/entity/Property.java
+++ alquila-seguro-backend/src/main/java/com/example/alquila_seguro_backend/entity/Property.java
@@ -29,47 +29,47 @@ public class Property {
     @OneToMany(mappedBy = "property")
     private List<Consultancy> consultancies;
 
-    @NotBlank(message = "title is required")
-    @Size(min = 3, max = 100, message = "title must be between 3 and 100 characters")
+    @NotBlank(message = "Titulo obligatorio.")
+    @Size(min = 3, max = 100, message = "El titulo debe contener entre 3 y 100 caracteres.")
     @Column(nullable = false)
     private String title;
 
-    @NotBlank(message = "description is required")
-    @Size(min = 10, max = 500, message = "description must be between 10 and 500 characters")
+    @NotBlank(message = "Descripción obligatoria.")
+    @Size(min = 10, max = 500, message = "La descripción debe contener entre 10 y 500 caracteres.")
     @Column(nullable = false)
     private String description;
 
-    @NotBlank(message = "location is required")
+    @NotBlank(message = "Localización obligatoria.")
     @Column(nullable = false)
     private String location;
 
-    @DecimalMin(value = "0.0", inclusive = false, message = "price must be greater than 0")
+    @DecimalMin(value = "0.0", inclusive = false, message = "El precio debe ser mayor que 0.")
     @Column(nullable = false)
     private Double pricePerNight;
 
-    @NotBlank(message = "category is required")
-    @Size(min = 3, max = 50, message = "category must be between 3 and 50 characters")
+    @NotBlank(message = "Categoria obligatoria.")
+    @Size(min = 3, max = 50, message = "La categoria debe contener entre 3 y 50 caracteres.")
     @Column(nullable = false)
     private String category;
 
-    @DecimalMin(value = "-90.0", inclusive = true, message = "latitude cannot be less than 90")
-    @DecimalMax(value = "90.0", inclusive = true, message = "latitude cannot be greater than 90")
+    @DecimalMin(value = "-90.0", inclusive = true, message = "Latitud no puede ser menor a 90.")
+    @DecimalMax(value = "90.0", inclusive = true, message = "latitud no puede ser mayor que 90.")
     private BigDecimal latitude;
 
-    @DecimalMin(value = "-180.0", inclusive = true, message = "longitude cannot be less than -180")
-    @DecimalMax(value = "180.0", inclusive = true, message = "longitude cannot be greater than 180")
+    @DecimalMin(value = "-180.0", inclusive = true, message = "Longitud no puede menor que -180.")
+    @DecimalMax(value = "180.0", inclusive = true, message = "Longitud no puede ser mayor que 180.")
     private BigDecimal longitude;
 
-    @Min(value = 0, message = "number of rooms must be greater than or equal to 0")
+    @Min(value = 0, message = "El número de habitaciones debe ser mayor o igual que 0.")
     private int numberOfRooms;
 
-    @Min(value = 0, message = "number of bathrooms must be greater than or equal to 0")
+    @Min(value = 0, message = "El número de baños debe ser mayor o igual que 0.")
     private int numberOfBathrooms;
 
-    @DecimalMin(value = "0.0", inclusive = false, message = "m² size must be greater than 0")
+    @DecimalMin(value = "0.0", inclusive = false, message = "El tamaño en m² debe ser mayor que 0.")
     private BigDecimal size;
 
-    @NotEmpty(message = "must have at least one feature")
+    @NotEmpty(message = "Debe tener al menos una caracteristica.")
     @ElementCollection
     @CollectionTable(name = "property_features", joinColumns = @JoinColumn(name = "property_id"))
     @Column(name = "feature")
@@ -84,8 +84,15 @@ public class Property {
     @Column(nullable = false)
     private String imageUrl;
 
+    @NotNull(message = "Estado de propiedad obligatorio.")
     @Column(nullable = false)
-    private boolean available;
+    @Enumerated(EnumType.STRING)
+    private PropertyStatus propertyStatus;
+
+    @NotNull(message = "Tipo de propiedad obligatorio.")
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private PropertyType propertyType;
 
 
 }
