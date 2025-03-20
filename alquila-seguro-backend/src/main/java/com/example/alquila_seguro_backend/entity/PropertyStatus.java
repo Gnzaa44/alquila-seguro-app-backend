18be61e7e55cb2eb7b6c8a76ff5acaad78d858ab
Gnzaa44 <141690430+Gnzaa44@users.noreply.github.com>
2025-03-20 10:41:02 -0300

    Added PropertyStatus enum
    for all states of a property

---
+++ alquila-seguro-backend/src/main/java/com/example/alquila_seguro_backend/entity/PropertyStatus.java
@@ -0,0 +1,34 @@
+package com.example.alquila_seguro_backend.entity;
+
+/**
+ * Constantes con los estados de una propiedad.
+ */
+public enum PropertyStatus {
+    /**
+     * Propiedad disponible para la reserva.
+     * Utilizado para propiedades de reserva inmediata.
+     */
+    AVAILABLE,
+
+    /**
+     * Propiedad no disponible para la reserva.
+     */
+    UNAVAILABLE,
+
+    /**
+     * Se necesita una revision antes de poder estar disponible.
+     */
+    NEEDS_REVIEW,
+
+    /**
+     * Verificacion completada y lista para ser reservada.
+     * Utilizada para propiedades de consultoria requerida, post-revision.
+     */
+    VERIFIED_AVAILABLE,
+
+    /**
+     * Propiedad reservada.
+     */
+    RESERVED
+
+}
