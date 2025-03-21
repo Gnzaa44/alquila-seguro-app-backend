04ae6f4648028c6e9f889cdab6ccb50f2941bec5
Gnzaa44 <141690430+Gnzaa44@users.noreply.github.com>
2025-03-21 08:54:34 -0300

    Update Property and Reservation enums
    added javadoc

--- alquila-seguro-backend/src/main/java/com/example/alquila_seguro_backend/entity/PropertyType.java
+++ alquila-seguro-backend/src/main/java/com/example/alquila_seguro_backend/entity/PropertyType.java
@@ -1,6 +1,17 @@
 package com.example.alquila_seguro_backend.entity;
 
+/**
+ * Constantes que definen dos tipos de propiedades del sistema.
+ */
 public enum PropertyType {
+    /**
+     * Propiedad lista para la reserva inmediata.
+     */
     RESERVE_INMEDIATE,
+
+    /**
+     * Propiedad no verificada y que requiere de consultoria, antes de ser reservada.
+     *
+     */
     CONSULTANCY_REQUIRED
 }
--- alquila-seguro-backend/src/main/java/com/example/alquila_seguro_backend/entity/ReservationStatus.java
+++ alquila-seguro-backend/src/main/java/com/example/alquila_seguro_backend/entity/ReservationStatus.java
@@ -1,8 +1,28 @@
 package com.example.alquila_seguro_backend.entity;
 
+/**
+ *  Constantes con los distintos estados de una reserva.
+ */
 public enum ReservationStatus {
+    /**
+     *  La reserva ha sido creada, pero aún no ha sido confirmada.
+     * Puede significar que el cliente ha solicitado la reserva,
+     * pero aún no ha realizado el pago o que la confirmación está pendiente de revisión por parte del administrador.
+     */
     PENDING,
+
+    /**
+     * Este estado indica que la reserva ha sido confirmada con éxito.
+     * El cliente ha realizado el pago o la reserva ha sido aprobada por el administrador.
+     * La propiedad está ahora reservada para el cliente durante el período acordado.
+     */
     CONFIRMED,
+
+    /**
+     * Este estado indica que la reserva ha sido cancelada.
+     * La cancelación puede haber sido realizada por el cliente o por el administrador.
+     * La propiedad vuelve a estar disponible para otras reservas.
+     */
     CANCELLED,
     COMPLETED
 }
