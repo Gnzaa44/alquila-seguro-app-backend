d8b40e417aa8abd82a8a9f14d191061e508bab26
Gnzaa44 <141690430+Gnzaa44@users.noreply.github.com>
2025-03-21 08:54:06 -0300

    Update DocumentStatus
    added javadoc comments

--- alquila-seguro-backend/src/main/java/com/example/alquila_seguro_backend/entity/DocumentStatus.java
+++ alquila-seguro-backend/src/main/java/com/example/alquila_seguro_backend/entity/DocumentStatus.java
@@ -1,7 +1,22 @@
 package com.example.alquila_seguro_backend.entity;
 
+/**
+ * Constantes con los estados de los documentos,
+ * (Factura y contrato de caución).
+ */
 public enum DocumentStatus {
-    PENDING, // Documento generado, pero no enviado
-    SENT,    // Documento enviado por correo electrónico
-    ERROR    // Error al enviar el documento
+    /**
+     * Documento generado, pero no enviado.
+     */
+    PENDING,
+
+    /**
+     * Documento enviado por correo electrónico.
+     */
+    SENT,
+
+    /**
+     *  Error al enviar el documento
+     */
+    ERROR
 }
