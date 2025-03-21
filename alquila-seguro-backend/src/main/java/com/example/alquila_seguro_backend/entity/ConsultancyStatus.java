2cc2a22419f367aec42c7f2c021e29b570853163
Gnzaa44 <141690430+Gnzaa44@users.noreply.github.com>
2025-03-21 08:53:31 -0300

    Update ConsultancyStatus
    added javadoc comments

--- alquila-seguro-backend/src/main/java/com/example/alquila_seguro_backend/entity/ConsultancyStatus.java
+++ alquila-seguro-backend/src/main/java/com/example/alquila_seguro_backend/entity/ConsultancyStatus.java
@@ -1,7 +1,21 @@
 package com.example.alquila_seguro_backend.entity;
 
+/**
+ * Constantes con los estados de una consultoria.
+ */
 public enum ConsultancyStatus {
+    /**
+     * Consultoria a la espera de ser respondida por el veedor.
+     */
     PENDING,
+
+    /**
+     * Consultoria respondida por el veedor.
+     */
     RESPONDED,
+
+    /**
+     * Consultoria ya cerrada entre el cliente y el veedor.
+     */
     CLOSED
 }
