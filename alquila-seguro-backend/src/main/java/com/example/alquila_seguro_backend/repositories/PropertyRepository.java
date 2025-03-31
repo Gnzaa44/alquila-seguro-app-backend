d867f009a2a4f2cd8d197b966d2d6768fe963e32
Gnzaa44 <141690430+Gnzaa44@users.noreply.github.com>
2025-03-31 20:14:37 -0300

    Added new methods in property repo
    added findbystatus and findbytype

--- alquila-seguro-backend/src/main/java/com/example/alquila_seguro_backend/repositories/PropertyRepository.java
+++ alquila-seguro-backend/src/main/java/com/example/alquila_seguro_backend/repositories/PropertyRepository.java
@@ -1,6 +1,8 @@
 package com.example.alquila_seguro_backend.repositories;
 
 import com.example.alquila_seguro_backend.entity.Property;
+import com.example.alquila_seguro_backend.entity.PropertyStatus;
+import com.example.alquila_seguro_backend.entity.PropertyType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -8,7 +10,8 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 @Repository
 public interface PropertyRepository extends JpaRepository<Property, Long> {
-    List<Property> findByAvailableTrue();
+    List<Property> findByStatus(PropertyStatus status);
+    List<Property> findByType(PropertyType type);
     List<Property> findByCategory(String category);
     List<Property> findByPricePerNightLessThanEqual(Double maxPrice);
     List<Property> findByNumberOfRoomsGreaterThanEqual(int minRooms);
