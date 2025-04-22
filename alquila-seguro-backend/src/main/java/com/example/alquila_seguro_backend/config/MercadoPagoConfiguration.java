d58989450b3113a70f5c1fc9304c79c75eb94cf3
Gnzaa44 <141690430+Gnzaa44@users.noreply.github.com>
2025-04-22 19:54:19 -0300

    MercadoPago SDK  and config
    added mercado sdk and initialized in config

---
+++ alquila-seguro-backend/src/main/java/com/example/alquila_seguro_backend/config/MercadoPagoConfiguration.java
@@ -0,0 +1,34 @@
+package com.example.alquila_seguro_backend.config;
+
+import com.mercadopago.MercadoPagoConfig;
+import com.mercadopago.client.payment.PaymentClient;
+import com.mercadopago.client.preference.PreferenceClient;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MercadoPagoConfiguration {
+
+    @Value("${MERCADO_PAGO_ACCESS_TOKEN_TEST}")
+    private String accessToken;
+
+    @PostConstruct
+    public void initialize() {
+        MercadoPagoConfig.setAccessToken(accessToken);
+    }
+
+    @Bean
+    public PreferenceClient preferenceClient() {
+        MercadoPagoConfig.setAccessToken(accessToken); // Configura el access token globalmente
+        return new PreferenceClient();
+    }
+
+    @Bean
+    public PaymentClient paymentClient() {
+        MercadoPagoConfig.setAccessToken(accessToken); // Asegúrate de que el access token esté configurado
+        return new PaymentClient();
+    }
+}
+
