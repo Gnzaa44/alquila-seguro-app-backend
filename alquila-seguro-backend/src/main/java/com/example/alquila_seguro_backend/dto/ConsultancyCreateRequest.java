2becd2d33a0ad20ee78bce8eb7d743f12c40727a
Gnzaa44 <141690430+Gnzaa44@users.noreply.github.com>
2025-04-22 20:07:20 -0300

    Update Consultancy Request
    added new attributes

--- alquila-seguro-backend/src/main/java/com/example/alquila_seguro_backend/dto/ConsultancyCreateRequest.java
+++ alquila-seguro-backend/src/main/java/com/example/alquila_seguro_backend/dto/ConsultancyCreateRequest.java
@@ -1,9 +1,11 @@
 package com.example.alquila_seguro_backend.dto;
 
 import com.example.alquila_seguro_backend.entity.ConsultancyStatus;
+import com.example.alquila_seguro_backend.validation.ArgentinianPhoneNumber;
 import jakarta.persistence.Column;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -17,8 +19,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ConsultancyCreateRequest {
-    @NotNull(message = "ID del cliente obligatorio.")
-    private Long clientId;
+    @NotBlank(message = "Nombre obligatorio.")
+    @Size(min = 2, max = 50, message = "El nombre del cliente debe contener entre 2  y 50 caracteres.")
+    private String clientFirstName;
+
+    @NotBlank(message = "Apellido obligatorio.")
+    @Size(min = 2, max = 50, message = "El apellido del cliente debe contener entre 2  y 50 caracteres.")
+    private String clientLastName;
+
+    @NotBlank(message = "El email es obligatorio.")
+    @Email(message = "El email del cliente debe ser una direccion de correo valida.")
+    private String clientEmail;
+
+    @ArgentinianPhoneNumber(message = "El número de teléfono debe ser un número válido de Argentina.")
+    @NotBlank(message = "Numero de telefono obligatorio.")
+    private String clientPhone;
 
     @NotNull(message = "ID de la propiedad obligatorio.")
     private Long propertyId;
