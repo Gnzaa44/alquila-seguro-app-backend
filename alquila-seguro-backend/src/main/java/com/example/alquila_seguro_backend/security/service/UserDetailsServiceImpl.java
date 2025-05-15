c2c2012dc820e503cf2afece160e4e489336f67b
Gnzaa44 <141690430+Gnzaa44@users.noreply.github.com>
2025-05-15 10:15:20 -0300

    Update userdetailsimpl
    added logging

--- alquila-seguro-backend/src/main/java/com/example/alquila_seguro_backend/security/service/UserDetailsServiceImpl.java
+++ alquila-seguro-backend/src/main/java/com/example/alquila_seguro_backend/security/service/UserDetailsServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.alquila_seguro_backend.security.service;
 import com.example.alquila_seguro_backend.entity.Admin;
 import com.example.alquila_seguro_backend.repositories.AdminRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,19 +12,22 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
+import java.util.logging.Logger;
 
 @Service
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
     private final AdminRepository adminRepository;
+    private final static Logger logger =  Logger.getLogger(UserDetailsServiceImpl.class.getName());
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        logger.info("Autenticando al usuario: {} "+ username);
 
         Admin admin = adminRepository.findByUsername(username)
-                .orElseThrow(()-> new UsernameNotFoundException("Usuario con el nombre " + username + " no existe"));
+                .orElseThrow(()->
+                        new UsernameNotFoundException("Usuario con el nombre " + username + " no existe"));
 
-        return new User(admin.getUsername(), admin.getPassword(), Collections.singletonList(new SimpleGrantedAuthority("ROLE_ADMIN")));
-
+        return UserDetailsImpl.build(admin);
     }
 }
