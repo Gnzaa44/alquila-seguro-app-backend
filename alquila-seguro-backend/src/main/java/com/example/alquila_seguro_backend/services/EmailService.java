8cfcd949d79b8bc6f409d8a28c76e9ffd5d9cacc
Gnzaa44 <141690430+Gnzaa44@users.noreply.github.com>
2025-05-15 10:13:48 -0300

    Update email service
    (its working right now)

--- alquila-seguro-backend/src/main/java/com/example/alquila_seguro_backend/services/EmailService.java
+++ alquila-seguro-backend/src/main/java/com/example/alquila_seguro_backend/services/EmailService.java
@@ -1,25 +1,24 @@
 package com.example.alquila_seguro_backend.services;
 
-import jakarta.mail.Message;
+
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailException;
+import org.springframework.core.io.Resource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
 
 @Service
+@RequiredArgsConstructor
 public class EmailService {
     private final static Logger LOGGER = LoggerFactory.getLogger(EmailService.class.getName());
 
-    private JavaMailSender mailSender;
+    private final JavaMailSender mailSender;
 
     public void sendEmail(String to, String subject, String body) throws MessagingException {
         SimpleMailMessage message = new SimpleMailMessage();
@@ -28,14 +27,14 @@ public class EmailService {
         message.setText(body);
         mailSender.send(message);
     }
-    public void sendEmailWithAttachment(String to, String subject, String body, File attachment) {
+    public void sendEmailWithAttachment(String to, String subject, String body, Resource attachment) {
         MimeMessage mimeMessage = mailSender.createMimeMessage();
         try {
             MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
             mimeMessageHelper.setTo(to);
             mimeMessageHelper.setSubject(subject);
             mimeMessageHelper.setText(body);
-            mimeMessageHelper.addAttachment(attachment.getName(), attachment);
+            mimeMessageHelper.addAttachment(attachment.getFilename(), attachment);
             mailSender.send(mimeMessage);
             LOGGER.info("Email enviado a: {} ", to);
         } catch (MessagingException e) {
