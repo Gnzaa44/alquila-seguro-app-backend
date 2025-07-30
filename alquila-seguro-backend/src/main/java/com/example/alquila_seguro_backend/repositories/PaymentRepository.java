eed6c8b7568179afa9e9033c3caba389a2a53c84
Gnzaa44 <141690430+Gnzaa44@users.noreply.github.com>
2025-07-30 13:48:50 -0300

    update payment repo
    added more methods to handle payments

--- alquila-seguro-backend/src/main/java/com/example/alquila_seguro_backend/repositories/PaymentRepository.java
+++ alquila-seguro-backend/src/main/java/com/example/alquila_seguro_backend/repositories/PaymentRepository.java
@@ -1,10 +1,11 @@
 package com.example.alquila_seguro_backend.repositories;
 
+import com.example.alquila_seguro_backend.entity.ExternalEntityType;
 import com.example.alquila_seguro_backend.entity.Payment;
+import com.example.alquila_seguro_backend.entity.PaymentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,4 +14,16 @@ public interface PaymentRepository extends JpaRepository<Payment, Long> {
     Optional<Payment> findByConsultancyId(Long consultancyId);
     Optional<Payment> findByPaymentReference(String paymentReference);
     Optional<Payment> findByExternalId(String externalId);
+    Optional<Payment> findByPaymentIdMP(String paymentIdMP);
+    // Método para buscar un pago PENDING por su Reservation ID
+    Optional<Payment> findByReservationIdAndPaymentStatus(Long reservationId, PaymentStatus paymentStatus);
+
+    // Método para buscar un pago PENDING por su Consultancy ID
+    Optional<Payment> findByConsultancyIdAndPaymentStatus(Long consultancyId, PaymentStatus paymentStatus);
+
+    Optional<Payment> findFirstByReservationIdAndPaymentStatusOrderByPaymentDateDesc(Long reservationId, PaymentStatus paymentStatus);
+
+    Optional<Payment> findFirstByConsultancyIdAndPaymentStatusOrderByPaymentDateDesc(Long consultancyId, PaymentStatus paymentStatus);
+
+    Optional<Payment> findFirstByExternalIdAndExternalEntityTypeAndPaymentStatusOrderByPaymentDateDesc(String externalId, ExternalEntityType externalEntityType, PaymentStatus paymentStatus);
 }
