bdb199032191eb0d4213cd5cb2eaf405dc0aecad
Gnzaa44 <141690430+Gnzaa44@users.noreply.github.com>
2025-07-30 13:42:21 -0300

    Update propertystatus
    deleted needs_review and verified_available status (no uses)

--- alquila-seguro-backend/src/main/java/com/example/alquila_seguro_backend/entity/PropertyStatus.java
+++ alquila-seguro-backend/src/main/java/com/example/alquila_seguro_backend/entity/PropertyStatus.java
@@ -6,7 +6,6 @@ package com.example.alquila_seguro_backend.entity;
 public enum PropertyStatus {
     /**
      * Propiedad disponible para la reserva.
-     * Utilizado para propiedades de reserva inmediata.
      */
     AVAILABLE,
 
@@ -14,18 +13,6 @@ public enum PropertyStatus {
      * Propiedad no disponible para la reserva.
      */
     UNAVAILABLE,
-
-    /**
-     * Se necesita una revision antes de poder estar disponible.
-     * Utilizada para propiedades de consultoria requerida.
-     */
-    NEEDS_REVIEW,
-
-    /**
-     * Verificacion completada y lista para ser reservada.
-     * Utilizada para propiedades de consultoria requerida, post-revision.
-     */
-    VERIFIED_AVAILABLE,
 
     /**
      * Propiedad reservada.
